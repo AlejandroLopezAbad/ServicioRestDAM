85ff537851d895b04c34f3d8aeb679eac51e9e62
Alejandro Lopez <alexlopezabad13@gmail.com>
2022-05-30 19:51:43 +0200

    Initial commit

---
+++ src/main/java/com/example/serviciorestdam/ServicioRestDamApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviciorestdam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioRestDamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioRestDamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serviciorestdam/ServicioRestDamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviciorestdam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioRestDamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
